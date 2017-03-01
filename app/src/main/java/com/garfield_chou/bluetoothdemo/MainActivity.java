57c5f55e079913cd74c7f6f6b2e7711a77358b5a
garfield_chou <garfield_chou@asus.com>
2017-03-01 18:41:17 +0800

    Ask for turning on the bluetooth if it's not on
    1, Use BluetoothAdapter
    2, New activity to turn on the BT

--- app/src/main/java/com/garfield_chou/bluetoothdemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/bluetoothdemo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.garfield_chou.bluetoothdemo;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter BA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+
+        if (BA.isEnabled()) {
+
+            Toast.makeText(getApplicationContext(), "Bluetooth is on", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(i);
+
+            if (BA.isEnabled()) {
+
+                Toast.makeText(getApplicationContext(), "Bluetooth turned on", Toast.LENGTH_LONG).show();
+
+            }
+
+        }
     }
 }
