de597109551025b7bf4399c2eea97e5d4873f6ce
shaudai <shaudai0402@hotmail.com>
2017-03-05 12:25:12 +0800

    List the paired devices
    1. Get the paired devices with getBondedDevices() API
    2. Populate ArrayList with returned Set of BluetoothDevice
    3. ArrayList -> ArrayAdapter -> ListView

--- app/src/main/java/com/garfield_chou/bluetoothdemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/bluetoothdemo/MainActivity.java
@@ -1,11 +1,17 @@
 package com.garfield_chou.bluetoothdemo;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
 
         Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         startActivity(i);
+
+    }
+
+    public void viewPairedDevices (View view) {
+
+        Set<BluetoothDevice> pairDevices = BA.getBondedDevices();
+
+        ListView pairedDevicesListView = (ListView) findViewById(R.id.pairedDevicesListView);
+
+        ArrayList pairedDevicesArrayList = new ArrayList();
+
+        for (BluetoothDevice bluetoothDevice : pairDevices) {
+
+            pairedDevicesArrayList.add(bluetoothDevice.getName());
+
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pairedDevicesArrayList);
+
+        pairedDevicesListView.setAdapter(arrayAdapter);
 
     }
 
